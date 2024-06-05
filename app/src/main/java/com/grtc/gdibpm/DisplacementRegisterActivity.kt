6e32e4feced85b77512ef5ef56b85beba4abd085
EPPR27 <eddie230@hotmail.com>
2024-06-04 22:30:54 -0500

    first commit

---
+++ app/src/main/java/com/grtc/gdibpm/DisplacementRegisterActivity.kt
@@ -0,0 +1,10 @@
+package com.grtc.gdibpm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DisplacementRegisterActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
