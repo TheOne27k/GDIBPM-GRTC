90ee18b66342af20e9e532c65ac1862fa84eb2cb
EPPR27 <eddie230@hotmail.com>
2024-06-12 20:26:44 -0500

    feat: implement return view to activity_heritage_asset_register.xml and center layout

--- app/src/main/java/com/grtc/gdibpm/HeritageAssetRegisterActivity.kt
+++ app/src/main/java/com/grtc/gdibpm/HeritageAssetRegisterActivity.kt
@@ -1,10 +1,13 @@
 package com.grtc.gdibpm
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class HeritageAssetRegisterActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_heritage_asset_register)
     }
 }
