47bab782cd8b092a951308bab3dd88f17d3d5b2e
kevin <021kevincastro@gmail.com>
2024-06-14 23:05:11 -0500

    feat:feat: create Forgot Password Activity

---
+++ app/src/main/java/com/grtc/gdibpm/ForgotPasswordActivity.kt
@@ -0,0 +1,14 @@
+package com.grtc.gdibpm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ForgotPasswordActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+
+
+}
