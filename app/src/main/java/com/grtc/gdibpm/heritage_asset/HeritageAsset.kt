94d8ac8da83d5e793a86f90d8022c724db4cdac5
EPPR27 <eddie230@hotmail.com>
2024-06-20 19:13:05 -0500

    feat: implement columnInfo, names to attributes of entity

--- app/src/main/java/com/grtc/gdibpm/heritage_asset/HeritageAsset.kt
+++ app/src/main/java/com/grtc/gdibpm/heritage_asset/HeritageAsset.kt
@@ -7,16 +7,24 @@ import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "heritage_asset_table",
-    indices = [Index(value = ["HeritageCode"], unique = true)]
+    indices = [Index(value = ["heritage_code"], unique = true)]
 )
 data class HeritageAsset (
+    @ColumnInfo(name = "heritage_code")
     val HeritageCode: String,
+    @ColumnInfo(name = "heritage_name")
     val HeritageName: String,
+    @ColumnInfo(name = "heritage_brand")
     val HeritageBrand: String,
+    @ColumnInfo(name = "heritage_model")
     val HeritageModel: String,
+    @ColumnInfo(name = "heritage_serial")
     val HeritageSerial: String,
-    val HeritageColoe: String,
+    @ColumnInfo(name = "heritage_color")
+    val HeritageColor: String,
+    @ColumnInfo(name = "heritage_state")
     var HeritageState: HeritageState,
+    @ColumnInfo(name = "heritage_evidence")
     var HeritageEvidence: Int
 ){
     @PrimaryKey(autoGenerate = true)
