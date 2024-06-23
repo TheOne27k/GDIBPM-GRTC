63c8d2c41e8ee6e0b1432c237727e2cff88849fa
EPPR27 <eddie230@hotmail.com>
2024-06-23 16:54:41 -0500

    fix: error with the implement database

--- app/src/main/java/com/grtc/gdibpm/displacement/DisplacementRepository.kt
+++ app/src/main/java/com/grtc/gdibpm/displacement/DisplacementRepository.kt
@@ -2,14 +2,13 @@ package com.grtc.gdibpm.displacement
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import com.grtc.gdibpm.database.database
+import com.grtc.gdibpm.database.GrtcDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class DisplacementRepository(application: Application) {
 
-    private val displacementDAO = database.getInstance(application).displacementDAO()
-
+    private val displacementDAO = GrtcDatabase.getInstance(application).displacementDAO()
     fun getDisplacements(): LiveData<List<Displacement>>{
         return displacementDAO.list()
     }
