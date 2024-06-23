4b09959aa93c6b388f6a66c03519dcd432547ba1
EPPR27 <eddie230@hotmail.com>
2024-06-23 16:53:33 -0500

    feat: implement entity user and displacement in the database

--- app/src/main/java/com/grtc/gdibpm/database/GrtcDatabase.kt
+++ app/src/main/java/com/grtc/gdibpm/database/GrtcDatabase.kt
@@ -4,14 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.grtc.gdibpm.displacement.Displacement
+import com.grtc.gdibpm.displacement.DisplacementDAO
 import com.grtc.gdibpm.heritage_asset.HeritageAsset
 import com.grtc.gdibpm.heritage_asset.HeritageDAO
+import com.grtc.gdibpm.users.User
 import com.grtc.gdibpm.users.UserDAO
 
-@Database(entities = [HeritageAsset::class], version = 2)
+@Database(entities = [HeritageAsset::class, User::class, Displacement::class], version = 2)
 abstract class GrtcDatabase: RoomDatabase() {
     abstract fun heritageDao(): HeritageDAO
-    abstract fun userDao(): HeritageDAO
+    abstract fun userDao(): UserDAO
+    abstract fun displacementDAO(): DisplacementDAO
+
     companion object {
         const val DATABASE_NAME = "grtc_displacement"
         @Volatile
